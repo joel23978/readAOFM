6f669853a54fa7cd884d729b879059c6e42ee3a3
joelfindlay <joel.findlay@gmail.com>
2023-09-14 01:03:17 +1000

    more chart examples

--- example_charts.R
+++ example_charts.R
@@ -5,10 +5,10 @@ source(here::here("script", "tidy_data.R"))
 
 view(browse_tables())
 
+read_transactional("transactional_tb_issuance_tender_syn")
 
-read_transactional("transactional_tb_issuance_tender_syn"
-)
 
+## Treasury Bond Coverage Ratio's
 p <- transactional_tb_issuance_tender_syn %>%
   filter(name == "coverage_ratio") %>%
   ggplot(aes(x = date_held
@@ -21,6 +21,22 @@ p <- transactional_tb_issuance_tender_syn %>%
   ylab("Coverage ratio") + 
   guides(colour=guide_legend(title="Tenor (yrs)")) +
   ggtitle("AOFM Treasury Bond Issuance - Coverage Ratio")
-
 p
+
+
+## Treasury Bond Annual Issuance Volume
+q <- transactional_tb_issuance_tender_syn %>%
+  filter(name == "amount_allotted") %>%
+  mutate(year = year(date_held)) %>%
+  group_by(year) %>%
+  summarise(value = sum(value)) %>%
+  ggplot(aes(x = year
+             , y = value
+  )) +
+  geom_col() +
+  theme_minimal() +
+  xlab("Issuance date") +
+  ylab("Issunace Volume") + 
+  ggtitle("AOFM Treasury Bond Issuance - Annual Volume")
+q
 
